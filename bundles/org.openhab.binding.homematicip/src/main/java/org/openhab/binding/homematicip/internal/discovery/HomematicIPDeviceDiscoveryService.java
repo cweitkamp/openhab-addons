6d3f17276d37c0ba0a440f7fac7a359e06fa852f
Christoph Weitkamp <github@christophweitkamp.de>
2025-10-23 11:38:20 +0200

    Fixed discovery service
    Signed-off-by: Christoph Weitkamp <github@christophweitkamp.de>

--- bundles/org.openhab.binding.homematicip/src/main/java/org/openhab/binding/homematicip/internal/discovery/HomematicIPDeviceDiscoveryService.java
+++ bundles/org.openhab.binding.homematicip/src/main/java/org/openhab/binding/homematicip/internal/discovery/HomematicIPDeviceDiscoveryService.java
@@ -18,17 +18,17 @@ import java.util.Map;
 import java.util.Set;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
-import org.eclipse.jdt.annotation.Nullable;
 import org.openhab.binding.homematicip.internal.handler.HomematicIPAccessPointHandler;
-import org.openhab.core.config.discovery.AbstractDiscoveryService;
+import org.openhab.core.config.discovery.AbstractThingHandlerDiscoveryService;
 import org.openhab.core.config.discovery.DiscoveryResult;
 import org.openhab.core.config.discovery.DiscoveryResultBuilder;
 import org.openhab.core.config.discovery.DiscoveryService;
 import org.openhab.core.thing.Thing;
 import org.openhab.core.thing.ThingTypeUID;
 import org.openhab.core.thing.ThingUID;
-import org.openhab.core.thing.binding.ThingHandler;
-import org.openhab.core.thing.binding.ThingHandlerService;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ServiceScope;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -40,9 +40,10 @@ import com.google.gson.JsonObject;
  *
  * @author Christoph Weitkamp - Initial contribution
  */
+@Component(scope = ServiceScope.PROTOTYPE, service = HomematicIPDeviceDiscoveryService.class)
 @NonNullByDefault
-public class HomematicIPDeviceDiscoveryService extends AbstractDiscoveryService
-        implements DiscoveryService, ThingHandlerService {
+public class HomematicIPDeviceDiscoveryService
+        extends AbstractThingHandlerDiscoveryService<HomematicIPAccessPointHandler> implements DiscoveryService {
 
     private static final String ENERGY_SENSOR_IEC = "ES_IEC";
     private static final String ENERGY_SENSOR_GAS = "ES_GAS";
@@ -68,54 +69,27 @@ public class HomematicIPDeviceDiscoveryService extends AbstractDiscoveryService
 
     private final Logger logger = LoggerFactory.getLogger(HomematicIPDeviceDiscoveryService.class);
 
-    private @Nullable HomematicIPAccessPointHandler accessPointHandler;
-
+    @Activate
     public HomematicIPDeviceDiscoveryService() {
-        super(SUPPORTED_THING_TYPES, TIMEOUT, false);
+        super(HomematicIPAccessPointHandler.class, SUPPORTED_THING_TYPES, TIMEOUT);
     }
 
     @Override
-    public void setThingHandler(@Nullable ThingHandler handler) {
-        if (handler instanceof HomematicIPAccessPointHandler) {
-            accessPointHandler = (HomematicIPAccessPointHandler) handler;
-        }
+    public void initialize() {
+        super.initialize();
+        thingHandler.registerDiscoveryService(this);
     }
 
     @Override
-    public @Nullable ThingHandler getThingHandler() {
-        return accessPointHandler;
-    }
-
-    @Override
-    public void activate() {
-        super.activate(null);
-        final HomematicIPAccessPointHandler handler = accessPointHandler;
-        if (handler != null) {
-            handler.registerDiscoveryService(this);
-        }
-    }
-
-    @Override
-    public void deactivate() {
-        final HomematicIPAccessPointHandler handler = accessPointHandler;
-        if (handler != null) {
-            handler.unregisterDiscoveryListener();
-        }
-        super.deactivate();
-    }
-
-    @Override
-    public Set<ThingTypeUID> getSupportedThingTypes() {
-        return SUPPORTED_THING_TYPES;
+    public void dispose() {
+        thingHandler.unregisterDiscoveryListener();
+        super.dispose();
     }
 
     @Override
     public void startScan() {
         logger.debug("Start manual homematic IP devices scan.");
-        final HomematicIPAccessPointHandler handler = accessPointHandler;
-        if (handler != null) {
-            handler.updateData();
-        }
+        thingHandler.updateData();
     }
 
     @Override
@@ -127,113 +101,105 @@ public class HomematicIPDeviceDiscoveryService extends AbstractDiscoveryService
     public void onDeviceAdded(String deviceId, JsonObject device) {
         logger.trace("Discovered new device with data: {}", device);
 
-        final HomematicIPAccessPointHandler handler = accessPointHandler;
-        if (handler != null) {
-            ThingTypeUID thingTypeUID = null;
-            String type = device.get(PROPERTY_TYPE).getAsString();
-            switch (type) {
-                case ENERGY_SENSORS_INTERFACE:
-                    JsonObject functionalChannels = device.get(PROPERTY_FUNCTIONAL_CHANNELS).getAsJsonObject();
-                    JsonElement connectedEnergySensorType = functionalChannels
-                            .get(FUNCTIONAL_TYPE_CHANNEL_DEVICE_CHANNEL).getAsJsonObject()
-                            .get(PROPERTY_CONNECTED_ENERGY_SENSOR_TYPE);
-                    switch (connectedEnergySensorType.getAsString()) {
-                        case ENERGY_SENSOR_IEC:
-                            thingTypeUID = THING_TYPE_ENERGY_SENSORS_INTERFACE;
-                            break;
-                        case ENERGY_SENSOR_GAS:
-                            thingTypeUID = THING_TYPE_GAS_SENSORS_INTERFACE;
-                            break;
-                        default:
-                            logger.debug(
-                                    "Discovered unknown energy sensor interface type '{}'. Please contact the binding developer.",
-                                    connectedEnergySensorType);
-                    }
-                    break;
-                case FLOOR_TERMINAL_BLOCK_12:
-                    thingTypeUID = THING_TYPE_UNDERFLOOR_HEATING_ACTUATOR;
-                    break;
-                case HEATING_THERMOSTAT:
-                    thingTypeUID = THING_TYPE_THERMOSTAT;
-                    break;
-                case HEATING_THERMOSTAT_EVO:
-                    thingTypeUID = THING_TYPE_THERMOSTAT_EVO;
-                    break;
-                case PLUGABLE_SWITCH_MEASURING:
-                    thingTypeUID = THING_TYPE_OUTLET;
-                    break;
-                case PRESENCE_DETECTOR_INDOOR:
-                    thingTypeUID = THING_TYPE_PRESENCE_SENSOR;
-                    break;
-                case SHUTTER_CONTACT_INVISIBLE:
-                case SHUTTER_CONTACT_MAGNETIC:
-                    thingTypeUID = THING_TYPE_SHUTTER_CONTACT;
-                    break;
-                case WALL_MOUNTED_THERMOSTAT_PRO:
-                    thingTypeUID = THING_TYPE_WALL_MOUNTED_THERMOSTAT;
-                    break;
-                default:
-                    logger.debug("Discovered unknown device of type '{}'. Please contact the binding developer.", type);
-            }
+        ThingTypeUID thingTypeUID = null;
+        String type = device.get(PROPERTY_TYPE).getAsString();
+        switch (type) {
+            case ENERGY_SENSORS_INTERFACE:
+                JsonObject functionalChannels = device.get(PROPERTY_FUNCTIONAL_CHANNELS).getAsJsonObject();
+                JsonElement connectedEnergySensorType = functionalChannels.get(FUNCTIONAL_TYPE_CHANNEL_DEVICE_CHANNEL)
+                        .getAsJsonObject().get(PROPERTY_CONNECTED_ENERGY_SENSOR_TYPE);
+                switch (connectedEnergySensorType.getAsString()) {
+                    case ENERGY_SENSOR_IEC:
+                        thingTypeUID = THING_TYPE_ENERGY_SENSORS_INTERFACE;
+                        break;
+                    case ENERGY_SENSOR_GAS:
+                        thingTypeUID = THING_TYPE_GAS_SENSORS_INTERFACE;
+                        break;
+                    default:
+                        logger.debug(
+                                "Discovered unknown energy sensor interface type '{}'. Please contact the binding developer.",
+                                connectedEnergySensorType);
+                }
+                break;
+            case FLOOR_TERMINAL_BLOCK_12:
+                thingTypeUID = THING_TYPE_UNDERFLOOR_HEATING_ACTUATOR;
+                break;
+            case HEATING_THERMOSTAT:
+                thingTypeUID = THING_TYPE_THERMOSTAT;
+                break;
+            case HEATING_THERMOSTAT_EVO:
+                thingTypeUID = THING_TYPE_THERMOSTAT_EVO;
+                break;
+            case PLUGABLE_SWITCH_MEASURING:
+                thingTypeUID = THING_TYPE_OUTLET;
+                break;
+            case PRESENCE_DETECTOR_INDOOR:
+                thingTypeUID = THING_TYPE_PRESENCE_SENSOR;
+                break;
+            case SHUTTER_CONTACT_INVISIBLE:
+            case SHUTTER_CONTACT_MAGNETIC:
+                thingTypeUID = THING_TYPE_SHUTTER_CONTACT;
+                break;
+            case WALL_MOUNTED_THERMOSTAT_PRO:
+                thingTypeUID = THING_TYPE_WALL_MOUNTED_THERMOSTAT;
+                break;
+            default:
+                logger.debug("Discovered unknown device of type '{}'. Please contact the binding developer.", type);
+        }
 
-            if (thingTypeUID != null) {
-                ThingUID bridgeUID = handler.getThing().getUID();
-                ThingUID thingUID = new ThingUID(thingTypeUID, bridgeUID, deviceId);
-                Map<String, Object> properties = Map.of( //
-                        THING_PROPERTY_ID, deviceId, //
-                        Thing.PROPERTY_VENDOR, device.get(PROPERTY_OEM).getAsString(), //
-                        Thing.PROPERTY_MODEL_ID, device.get(PROPERTY_MODEL_TYPE).getAsString(), //
-                        Thing.PROPERTY_FIRMWARE_VERSION, device.get(PROPERTY_FIRMWARE_VERSION).getAsString() //
-                );
+        if (thingTypeUID != null) {
+            ThingUID bridgeUID = thingHandler.getThing().getUID();
+            ThingUID thingUID = new ThingUID(thingTypeUID, bridgeUID, deviceId);
+            Map<String, Object> properties = Map.of( //
+                    THING_PROPERTY_ID, deviceId, //
+                    Thing.PROPERTY_VENDOR, device.get(PROPERTY_OEM).getAsString(), //
+                    Thing.PROPERTY_MODEL_ID, device.get(PROPERTY_MODEL_TYPE).getAsString(), //
+                    Thing.PROPERTY_FIRMWARE_VERSION, device.get(PROPERTY_FIRMWARE_VERSION).getAsString() //
+            );
 
-                DiscoveryResult discoveryResult = DiscoveryResultBuilder.create(thingUID).withProperties(properties)
-                        .withRepresentationProperty(THING_PROPERTY_ID).withBridge(bridgeUID)
-                        .withLabel(device.get(PROPERTY_LABEL).getAsString()).build();
+            DiscoveryResult discoveryResult = DiscoveryResultBuilder.create(thingUID).withProperties(properties)
+                    .withRepresentationProperty(THING_PROPERTY_ID).withBridge(bridgeUID)
+                    .withLabel(device.get(PROPERTY_LABEL).getAsString()).build();
 
-                thingDiscovered(discoveryResult);
-            }
+            thingDiscovered(discoveryResult);
         }
     }
 
     public void onGroupAdded(String groupId, JsonObject group) {
         logger.trace("Discovered new group with data: {}", group);
 
-        final HomematicIPAccessPointHandler handler = accessPointHandler;
-        if (handler != null) {
-            ThingTypeUID thingTypeUID = null;
-            String discoveryResultLabel = "";
-            String type = group.get(PROPERTY_TYPE).getAsString();
-            switch (type) {
-                case HEATING_GROUP:
-                    discoveryResultLabel = "homematic IP Heizgruppe - " + group.get(PROPERTY_LABEL).getAsString();
-                    thingTypeUID = THING_TYPE_HEATING_GROUP;
-                    break;
-                case SECURITY_ZONE_GROUP:
-                    // this group can be skipped as it will be handled by the Access Point Handler in a separate Channel
-                    // 'alarm-mode'
-                    break;
-                case META_GROUP:
-                case SECURITY_GROUP:
-                case SWITCHING_GROUP:
-                    // TODO implement META_GROUP, SECURITY_GROUP and/or SWITCHING_GROUP
-                    break;
-                default:
-                    logger.debug("Discovered unknown group of type '{}'. Please contact the binding developer.", type);
-            }
+        ThingTypeUID thingTypeUID = null;
+        String discoveryResultLabel = "";
+        String type = group.get(PROPERTY_TYPE).getAsString();
+        switch (type) {
+            case HEATING_GROUP:
+                discoveryResultLabel = "homematic IP Heizgruppe - " + group.get(PROPERTY_LABEL).getAsString();
+                thingTypeUID = THING_TYPE_HEATING_GROUP;
+                break;
+            case SECURITY_ZONE_GROUP:
+                // this group can be skipped as it will be handled by the Access Point Handler in a separate Channel
+                // 'alarm-mode'
+                break;
+            case META_GROUP:
+            case SECURITY_GROUP:
+            case SWITCHING_GROUP:
+                // TODO implement META_GROUP, SECURITY_GROUP and/or SWITCHING_GROUP
+                break;
+            default:
+                logger.debug("Discovered unknown group of type '{}'. Please contact the binding developer.", type);
+        }
 
-            if (thingTypeUID != null) {
-                ThingUID bridgeUID = handler.getThing().getUID();
-                ThingUID thingUID = new ThingUID(thingTypeUID, bridgeUID, groupId);
-                Map<String, Object> properties = Map.of(THING_PROPERTY_ID, groupId);
+        if (thingTypeUID != null) {
+            ThingUID bridgeUID = thingHandler.getThing().getUID();
+            ThingUID thingUID = new ThingUID(thingTypeUID, bridgeUID, groupId);
+            Map<String, Object> properties = Map.of(THING_PROPERTY_ID, groupId);
 
-                // TODO: translate label of group
-                DiscoveryResult discoveryResult = DiscoveryResultBuilder.create(thingUID).withProperties(properties)
-                        .withRepresentationProperty(THING_PROPERTY_ID).withBridge(bridgeUID)
-                        .withLabel(discoveryResultLabel).build();
+            // TODO: translate label of group
+            DiscoveryResult discoveryResult = DiscoveryResultBuilder.create(thingUID).withProperties(properties)
+                    .withRepresentationProperty(THING_PROPERTY_ID).withBridge(bridgeUID).withLabel(discoveryResultLabel)
+                    .build();
 
-                thingDiscovered(discoveryResult);
-            }
-
+            thingDiscovered(discoveryResult);
         }
     }
 }
